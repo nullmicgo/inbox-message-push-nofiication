e6f4aab19917e591b41c2259159eeb1c32577a29
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-03-07 17:18:57 +0800

    updated the port
    updated the port

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+
+
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
